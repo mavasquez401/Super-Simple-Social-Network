f0a2aed0e966f9b86eb7f247951f305cca1c2fe4
mavasquez401 <mavasquez401@gmail.com>
2024-06-04 13:54:58 -0400

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import { useState } from "react";
 
+const rooms = [
+  { type: "Single", dailyRate: 50, available: true },
+  { type: "Double", dailyRate: 80, available: true },
+  { type: "Suite", dailyRate: 150, available: true },
+  { type: "Deluxe", dailyRate: 200, available: false },
+  { type: "Penthouse", dailyRate: 300, available: false },
+];
+
 function App() {
+  const [selectedRoom, setSelectedRoom] = useState("");
+
   return <></>;
 }
 
